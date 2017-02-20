3e0c2eec34ab1a46a1be3276aec3972923484993
Domenger Amélie <amelie.domenger@etu.u-bordeaux.fr>
2017-02-21 00:19:22 +0100

    create arithmetique + change Af to Sk

--- compIMP.c
+++ compIMP.c
@@ -2,7 +2,7 @@
 #include "AST.h"
 #include "imp.tab.h"
 #include "environ.h"
-static int currentV=0,currentC=0,current=0;
+static int currentV=0,currentC=0,current=0,testPl=0,leftCurrent=0,test=0,testArith=0;;
 
 void print(int etq,const char *op, const char *arg, const char *arg2, const char *des) {
     printf("ET%d\t:%s\t:%s\t:%s\t:%s\n",etq,op,arg ? arg : "",arg2 ? arg2 : "",des ? des : "");
@@ -14,14 +14,17 @@ int ex(ENV *e,nodeType *p) {
     char buf[20];
     char buf2[20];
     char buf3[20];
+    char bufVar[20];
 	if (!p) return 0;
 	switch(p->type) {
 	    case typeCon:   snprintf(buf,20,"%d",p->con.value);
 	                    snprintf(buf2,20,"CT%d",++currentC);
-	                    print(current++,"Afc", buf, NULL, buf2);
+                        print(current++,"Afc", buf, NULL, buf2);
+                        test=0;
 	                    break;
 	    case typeId:	snprintf(buf2,20,"CT%d",++currentC);
-	                    print(current++,"Af", buf2, p->id.id, NULL);
+	                    print(current++,"Sk", NULL, NULL, p->id.id);
+                        test=1;
 	                    break;
 	    case typeOpr:    
 	    switch(p->opr.oper) {
@@ -29,8 +32,13 @@ int ex(ENV *e,nodeType *p) {
 	                    printf("%s\t:%s\t:%s\t:%s\t:%s\n",buf,"Sk","","","");
 	                    ex(e,p->opr.op[0]);
 	                    snprintf(buf2,20,"CT%d",currentC);
-	                    snprintf(buf3,20,"JMP%d",lbJMP2 = lbJMP++);
-                        print(current++,"Jz",buf2,NULL,buf3);
+                        snprintf(buf3,20,"JMP%d",lbJMP2 = lbJMP++);
+                        if(test==0){
+                            print(current++,"Jz",buf2,NULL,buf3);
+                        }else{
+                            print(current++,"Jz",p->opr.op[0]->id.id,NULL,buf3);
+                            test=0;
+                        }
                         ex(e,p->opr.op[1]);
                         print(current++,"Je",NULL,NULL,buf);
                         printf("%s\t:%s\t:%s\t:%s\t:%s\n",buf3,"Sk","","","");
@@ -40,7 +48,12 @@ int ex(ENV *e,nodeType *p) {
             case If:    ex(e,p->opr.op[0]);
                         snprintf(buf,20,"CT%d",currentC);
                         snprintf(buf2,20,"JMP%d",lbJMP1 = lbJMP++);
-                        print(current++,"Jz",buf,NULL,buf2);
+                        if(test==0){
+                            print(current++,"Jz",buf,NULL,buf2);
+                        }else{
+                            print(current++,"Jz",p->opr.op[0]->id.id,NULL,buf2);
+                            test=0;
+                        }
                         ex(e,p->opr.op[1]);
                         snprintf(buf3,20,"JMP%d",lbJMP2 = lbJMP++);
                         print(current++,"Jp",NULL,NULL,buf3);
@@ -49,21 +62,76 @@ int ex(ENV *e,nodeType *p) {
                         printf("%s\t:%s\t:%s\t:%s\t:%s\n",buf3,"Sk","","","");
 	                    return 0; 
 	        case Af:    ex(e,p->opr.op[1]);
-	                    snprintf(buf,20,"CT%d",currentC);
-	                    print(current++,"Af", p->opr.op[0]->id.id, buf,NULL );
+                        if (testPl == 0){
+                            if (test==0){
+                                snprintf(buf,20,"CT%d",currentC);
+                                print(current++,"Af", p->opr.op[0]->id.id, buf,NULL );
+                            }else{
+                                snprintf(buf,20,"CT%d",currentC);
+                                print(current++,"Af", p->opr.op[0]->id.id, p->opr.op[1]->id.id,NULL );
+                                test=1;
+                            }
+                        }else{
+                            if (test==0){
+                                snprintf(bufVar,20,"VA%d",currentC);
+                                print(current++,"Af", p->opr.op[0]->id.id, bufVar,NULL );
+                                testPl=0;
+                            }else{
+                                snprintf(bufVar,20,"VA%d",currentC);
+                                print(current++,"Af", p->opr.op[0]->id.id, bufVar,NULL );
+                                testPl=0;
+                                test=0;
+                            }
+                        }
 	                    break;
 	                    
 	        case Se:    ex(e,p->opr.op[0]); return ex(e,p->opr.op[1]);
 	        
 	        case Pl:    ex(e,p->opr.op[0]);
-                        int leftCurrent = currentC;
+                        leftCurrent = currentC;
                         ex(e,p->opr.op[1]);
                         snprintf(buf,20,"CT%d",leftCurrent);
                         snprintf(buf2,20,"CT%d",currentC);
-                        print(current++,"Pl",buf,buf2,buf2);
+                        snprintf(bufVar,20,"VA%d",++currentC);
+                        if (test==0){
+                            print(current++,"Pl",p->opr.op[0]->id.id,buf2,bufVar);
+                        }else{
+                            print(current++,"Pl",p->opr.op[0]->id.id,p->opr.op[1]->id.id,bufVar);
+
+                        }
+                        testPl=1;
                         break;
-	        case Mo:    return ex(e,p->opr.op[0]) - ex(e,p->opr.op[1]);
-	        case Mu:    return ex(e,p->opr.op[0]) * ex(e,p->opr.op[1]);
+                
+            case Mo:    ex(e,p->opr.op[0]);
+                        leftCurrent = currentC;
+                        ex(e,p->opr.op[1]);
+                        snprintf(buf,20,"CT%d",leftCurrent);
+                        snprintf(buf2,20,"CT%d",currentC);
+                        snprintf(bufVar,20,"VA%d",++currentC);
+                        if (test==0){
+                            print(current++,"Mo",p->opr.op[0]->id.id,buf2,bufVar);
+                        }else{
+                            print(current++,"Mo",p->opr.op[0]->id.id,p->opr.op[1]->id.id,bufVar);
+                    
+                        }
+                        testPl=1;
+                        break;
+
+            
+            case Mu:    ex(e,p->opr.op[0]);
+                        leftCurrent = currentC;
+                        ex(e,p->opr.op[1]);
+                        snprintf(buf,20,"CT%d",leftCurrent);
+                        snprintf(buf2,20,"CT%d",currentC);
+                        snprintf(bufVar,20,"VA%d",++currentC);
+                        if (test==0){
+                            print(current++,"Mu",p->opr.op[0]->id.id,buf2,bufVar);
+                        }else{
+                            print(current++,"Mu",p->opr.op[0]->id.id,p->opr.op[1]->id.id,bufVar);
+                    
+                        }
+                        testPl=1;
+                        break;
 	    }
     }
     return 0;
